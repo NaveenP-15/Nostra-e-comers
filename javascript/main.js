eb49caf6bc6350ff80b90ae75b6932340319e29d
NaveenP-15 <159607927+NaveenP-15@users.noreply.github.com>
2024-04-16 17:13:42 +0530

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+var navMenu = document.querySelector(".nav-menu")
+var navToggle = document.querySelector(".nav-toggle")
+var navClose = document.querySelector(".nav-close")
+
+function shownavbar(){
+    navMenu.style.right="0"
+}
+function removenavbar(){
+    navMenu.style.right="-100%"
+}
